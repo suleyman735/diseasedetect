23d235506bb67bbaa7c48f60a3cd1c213f8444f6
Suleyman Aliyev <36112126+suleyman735@users.noreply.github.com>
2024-07-03 16:22:57 +0200

    some setting done

---
+++ disease/mask_rcnn_detection.py
@@ -0,0 +1,23 @@
+
+import os
+import sys
+# import numpy as np
+# import skimage.io
+# from mrcnn import utils
+# import mrcnn.model as modellib
+# from mrcnn.config import Config
+# from mrcnn import visualize
+
+def detect_and_compare():
+    print('hello baby')
+    pass
+    # Load image
+    # image = skimage.io.imread(image_path)
+
+    # # Run detection
+    # results = model.detect([image], verbose=1)
+
+    # # Visualize results
+    # r = results[0]
+    # visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+    #                             class_names, r['scores'])
--- disease/views.py
+++ disease/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from django.http import HttpResponse
 from django.views import View
-
+from .mask_rcnn_detection import detect_and_compare
+from .forms import ImageUploadForm
+from .models import ImageUpload
 
 
 class Disease(View):
+
     def get(self, request):
-        return HttpResponse('Hello, World!')
+        detect_and_compare()
+        
+        return render(request, 'index.html',)
+    
+    def post(self,request):
+        upload_image(request)
+        
+def upload_image(request):
+        if request.method == 'POST':
+            form = ImageUploadForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                
+                # detect_and_compare(image_upload.image.path)
+                return redirect('success')
+        else:
+            form = ImageUploadForm()
+        # return render(request, 'upload.html', {'form': form})
